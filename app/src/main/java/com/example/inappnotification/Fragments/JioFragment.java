8f431733b6a97941f11bbc9362e1a05e24570bd3
Adityakumar Yadav <adityakumar.y@somaiya.edu>
2019-12-01 20:04:15 +0530

    Added BottomSheetDialog for Alert Message

--- app/src/main/java/com/example/inappnotification/Fragments/JioFragment.java
+++ app/src/main/java/com/example/inappnotification/Fragments/JioFragment.java
@@ -1,32 +1,46 @@
 package com.example.inappnotification.Fragments;
 
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.inappnotification.R;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class JioFragment extends Fragment {
 
-
-    public JioFragment() {
-        // Required empty public constructor
-    }
-
+    @BindView(R.id.btnExit)
+    Button btnExit;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_jio, container, false);
+        View v = inflater.inflate(R.layout.fragment_jio, container, false);
+        ButterKnife.bind(this, v);
+        return v;
+    }
+
+    @OnClick(R.id.btnExit)
+    public void showDialog(){
+        BottomSheetDialog dialog = new BottomSheetDialog(getActivity());
+        dialog.setContentView(R.layout.alert_dialog_layout);
+        dialog.show();
     }
 
 }
